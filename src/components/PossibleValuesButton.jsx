ed4bfed6aec89993b46c854eafb0490af598e2c9
Michael Joshua Ramos <michael.joshua.millevo.ramos@gmail.com>
2024-07-31 15:45:35 +0800

    Add changes in PossibleValuesButton component

--- src/components/PossibleValuesButton.jsx
+++ src/components/PossibleValuesButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ChevronIcon from "./ChevronIcon";
 
-const PossibleValuesButton = ({ showValues }) => {
+const PossibleValuesButton = ({ onClick }) => {
   return (
-    <button onClick={showValues}>
-      <div className="flex flex-row items-baseline space-x-1 rounded-[8px] border border-gray-200 bg-gray-50 px-2 py-0.5">
+    <button onClick={onClick}>
+      <div className="flex items-baseline space-x-1 rounded-lg border border-gray-200 bg-gray-50 px-2 py-0.5">
         <ChevronIcon />
         <p className="text-[14px] font-light text-gray-700">Possible Values</p>
       </div>
